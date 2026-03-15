11a0cdc85c471e7e0bb5774d44623b7ed661d42c
Lavanya Singh <lavanyasingh2203@gmail.com>
2026-03-15 22:17:47 +0530

    Add files via upload

---
+++ Day 41/Q1.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node *front = NULL, *rear = NULL;
+
+// Enqueue operation
+void enqueue(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if (rear == NULL) {
+        front = rear = newNode;
+        return;
+    }
+
+    rear->next = newNode;
+    rear = newNode;
+}
+
+// Dequeue operation
+void dequeue() {
+    if (front == NULL) {
+        printf("-1\n");
+        return;
+    }
+
+    struct Node* temp = front;
+    printf("%d\n", front->data);
+
+    front = front->next;
+
+    if (front == NULL)
+        rear = NULL;
+
+    free(temp);
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++) {
+        int choice, value;
+
+        scanf("%d", &choice);
+
+        if (choice == 1) {
+            scanf("%d", &value);
+            enqueue(value);
+        } 
+        else if (choice == 2) {
+            dequeue();
+        }
+    }
+
+    return 0;
+}
