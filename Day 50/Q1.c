40cf24fdc75040a96c2b67185b06dbe8f304f2b6
Lavanya Singh <lavanyasingh2203@gmail.com>
2026-03-23 23:34:20 +0530

    Add files via upload

---
+++ Day 50/Q1.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure of BST node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create new node
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// Insert into BST
+struct Node* insert(struct Node* root, int value) {
+    if (root == NULL)
+        return createNode(value);
+
+    if (value < root->data)
+        root->left = insert(root->left, value);
+    else if (value > root->data)
+        root->right = insert(root->right, value);
+
+    return root;
+}
+
+// Search in BST
+struct Node* search(struct Node* root, int key) {
+    if (root == NULL || root->data == key)
+        return root;
+
+    if (key < root->data)
+        return search(root->left, key);
+    else
+        return search(root->right, key);
+}
+
+// Main function
+int main() {
+    struct Node* root = NULL;
+
+    // Creating BST
+    root = insert(root, 4);
+    insert(root, 2);
+    insert(root, 7);
+    insert(root, 1);
+    insert(root, 3);
+
+    int key = 2;
+
+    struct Node* result = search(root, key);
+
+    if (result != NULL)
+        printf("Element %d found in BST\n", key);
+    else
+        printf("Element %d not found in BST\n", key);
+
+    return 0;
+}
