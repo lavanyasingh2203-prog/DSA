159e582c86e2f7766cd22068368fa753750d6f56
Lavanya Singh <lavanyasingh2203@gmail.com>
2026-03-24 23:16:02 +0530

    Add files via upload

---
+++ Day 51/Q1.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+// Create new node
+struct Node* createNode(int val) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Insert into BST
+struct Node* insert(struct Node* root, int val) {
+    if (root == NULL) return createNode(val);
+
+    if (val < root->data)
+        root->left = insert(root->left, val);
+    else
+        root->right = insert(root->right, val);
+
+    return root;
+}
+
+// Find LCA
+struct Node* findLCA(struct Node* root, int n1, int n2) {
+    if (root == NULL) return NULL;
+
+    if (n1 < root->data && n2 < root->data)
+        return findLCA(root->left, n1, n2);
+
+    if (n1 > root->data && n2 > root->data)
+        return findLCA(root->right, n1, n2);
+
+    return root; // This is LCA
+}
+
+int main() {
+    int n, val, x, y;
+    struct Node* root = NULL;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &val);
+        root = insert(root, val);
+    }
+
+    scanf("%d %d", &x, &y);
+
+    struct Node* lca = findLCA(root, x, y);
+
+    if (lca != NULL)
+        printf("%d\n", lca->data);
+
+    return 0;
+}
